efcb96cc4b144529b6739609b2b2a33c2318014c
yosefferri <joseferri225@gmail.com>
2021-10-23 08:30:00 -0300

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EventController;
 
-Route::prefix('events')->group(function () {
-    Route::get('/', [EventController::class, 'index']);
-    Route::post('/', [EventController::class, 'store']);
-    Route::get('/{event}', [EventController::class, 'show']);
-    Route::put('/{event}', [EventController::class, 'update']);
-    Route::delete('/{event}', [EventController::class, 'destroy']);
-});
-
 
 Route::post('registration', [AuthController::class, 'customRegistration']);
 Route::post('signout', [AuthController::class, 'signOut']);
@@ -19,4 +11,11 @@ Route::post('signout', [AuthController::class, 'signOut']);
 Route::get('auth/login', [AuthController::class, 'login']);
 Route::group(['middleware' => ['apiJWT']], function (){
     Route::get('/users/events', [EventController::class, 'userEvents']);
+    Route::prefix('events')->group(function () {
+        Route::get('/', [EventController::class, 'index']);
+        Route::post('/', [EventController::class, 'store']);
+        Route::get('/{event}', [EventController::class, 'show']);
+        Route::put('/{event}', [EventController::class, 'update']);
+        Route::delete('/{event}', [EventController::class, 'destroy']);
+    });
 });
